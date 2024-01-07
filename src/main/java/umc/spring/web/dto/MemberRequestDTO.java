93f270751529d04539970068fda39b5eddef6f61
Eunse Jang <senuej37@gmail.com>
2024-01-07 19:30:06 +0900

    feat: 회원가입에 Category validation 적용

--- src/main/java/umc/spring/web/dto/MemberRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -1,7 +1,11 @@
 package umc.spring.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import umc.spring.domain.enums.Gender;
+import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.List;
 
@@ -12,13 +16,21 @@ public class MemberRequestDTO {
 
         // TODO socialType, email
 
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @ExistCategories
         List<Long> preferCategory;
     }
 }
