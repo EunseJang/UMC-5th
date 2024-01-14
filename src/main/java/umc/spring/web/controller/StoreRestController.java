dfc6ce958222186e7e1e444d54ff0fb14c46df5e
Eunse Jang <senuej37@gmail.com>
2024-01-14 15:02:54 +0900

    style: service 패키지 명 변경

--- src/main/java/umc/spring/web/controller/StoreRestController.java
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Store;
-import umc.spring.service.StoreService.StoreCommandService;
+import umc.spring.service.storeService.StoreCommandService;
 import umc.spring.web.dto.StoreRequestDTO;
 import umc.spring.web.dto.StoreResponseDTO;
 
