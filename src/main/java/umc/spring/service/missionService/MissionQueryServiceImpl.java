295c088504dc6c5bcf28b9382ef04d01e87fe1a1
Eunse Jang <senuej37@gmail.com>
2024-01-17 23:39:29 +0900

    feat: 가게 미션 도전하기 API 로직 구현

---
+++ src/main/java/umc/spring/service/missionService/MissionQueryServiceImpl.java
@@ -0,0 +1,22 @@
+package umc.spring.service.missionService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring.Repository.MissionRepository;
+import umc.spring.domain.Mission;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MissionQueryServiceImpl implements MissionQueryService {
+
+    private final MissionRepository missionRepository;
+
+    @Override
+    public Optional<Mission> findMission(Long id) {
+        return missionRepository.findById(id);
+    }
+}
