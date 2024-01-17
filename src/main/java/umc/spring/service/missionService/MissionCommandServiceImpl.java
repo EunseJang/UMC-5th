17bbc614fbcdc22a650c0a4da243adcf51421c2d
Eunse Jang <senuej37@gmail.com>
2024-01-17 23:38:02 +0900

    style: 주석 추가

--- src/main/java/umc/spring/service/missionService/MissionCommandServiceImpl.java
+++ src/main/java/umc/spring/service/missionService/MissionCommandServiceImpl.java
@@ -3,7 +3,6 @@ package umc.spring.service.missionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import umc.spring.Repository.MemberRepository;
 import umc.spring.Repository.MissionRepository;
 import umc.spring.Repository.StoreRepository;
 import umc.spring.converter.MissionConverter;
@@ -18,7 +17,7 @@ public class MissionCommandServiceImpl implements MissionCommandService {
     private final MissionRepository missionRepository;
     private final StoreRepository storeRepository;
 
-
+    /** 미션 등록하기 */
     @Override
     @Transactional
     public Mission createMission(Long storeId, MissionRequestDTO.MissionDTO request) {
